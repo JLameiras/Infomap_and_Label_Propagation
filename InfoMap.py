0c0119e337a94f8706ef8b2910584043c08d3e3c
Pedro Lameiras <pedrolameiras@tecnico.ulisboa.pt>
2023-10-18 02:23:44 +0100

    identation

--- InfoMap.py
+++ InfoMap.py
@@ -22,12 +22,12 @@ class Graph:
 
         return self.graph
     
-        def createGraphLFR(self, n, tau1, tau2, mu, average_degree, min_degree, max_degree,
+    def createGraphLFR(self, n, tau1, tau2, mu, average_degree, min_degree, max_degree,
                        min_community, max_community, tol, max_iters, seed):
             
-            self.graph = networkx.LFR_benchmark_graph(n, tau1, tau2, mu, average_degree, min_degree, max_degree,min_community, max_community, tol, max_iters, seed)
+        self.graph = networkx.LFR_benchmark_graph(n, tau1, tau2, mu, average_degree, min_degree, max_degree,min_community, max_community, tol, max_iters, seed)
 
-            return self.graph
+        return self.graph
 
 
 class InfoMap:
